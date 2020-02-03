eeeb370a71686da6c17b19d20ad09b0252840100
mdebug <mdebug@gmail.com>
2020-02-03 14:31:12 -0500

    debuged

---
+++ Test-Repo/script.py
@@ -0,0 +1,25 @@
+import math
+import os
+import sys
+from os import rename
+
+import requests
+
+
+print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+print(greet('World'))
+print(greet('Corey'))
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
+
+#for input you need to run in terminal.
+name=input("Your Name? ")
+print ('Hello', name)
+
